a6cd67df63f90f79b0025a808c12356356fc1ca4
godheaven007 <345777292@qq.com>
2021-01-29 15:24:20 +0800

    加减乘除算法添加

--- src/util/index.js
+++ src/util/index.js
@@ -41,5 +41,76 @@ export default {
         str += '';
         while (str.length < len) str = '0' + str;
         return str;
+    },
+
+    accAdd(arg1, arg2) {
+        var r1, r2, m;
+        try {
+            r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+            r1 = 0;
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+            r2 = 0;
+        }
+        m = Math.pow(10, Math.max(r1, r2));
+
+        var mul1 = accMul(arg1, m),
+            mul2 = accMul(arg2, m);
+        return accDiv(mul1 + mul2, m);
+        // return (arg1 * m + arg2 * m) / m;
+    },
+    accSub(arg1, arg2, fixed) {
+        var r1, r2, m, n;
+        try {
+            r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+            r1 = 0;
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+            r2 = 0;
+        }
+        m = Math.pow(10, Math.max(r1, r2));
+        if(typeof fixed != 'undefined') {
+            n = fixed;
+        } else {
+            //动态控制精度长度
+            n = (r1 >= r2) ? r1 : r2;
+        }
+
+        return ((arg2 * m - arg1 * m) / m).toFixed(n);
+    },
+    accMul(arg1, arg2) {
+        var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+        try {
+            m += s1.split(".")[1].length
+        } catch (e) {
+        }
+        try {
+            m += s2.split(".")[1].length
+        } catch (e) {
+        }
+        return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+    },
+    accDiv(arg1, arg2) {
+        var t1 = 0, t2 = 0, r1, r2;
+        try {
+            t1 = arg1.toString().split(".")[1].length
+        } catch (e) {
+        }
+        try {
+            t2 = arg2.toString().split(".")[1].length
+        } catch (e) {
+        }
+        with (Math) {
+            r1 = Number(arg1.toString().replace(".", ""));
+            r2 = Number(arg2.toString().replace(".", ""));
+            return (r1 / r2) * pow(10, t2 - t1);
+        }
     }
+
 }
