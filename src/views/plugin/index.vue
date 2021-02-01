44f7a7f7d8093d146ba164ee4f8884d9bb0c8c0a
godheaven007 <345777292@qq.com>
2021-02-01 11:28:51 +0800

    插件页面完成

--- src/views/plugin/index.vue
+++ src/views/plugin/index.vue
@@ -1,13 +1,174 @@
 <template>
-  <div>插件市场</div>
+  <div class="plugin-wrap">
+    <div class="plugin-title">
+      <div class="title">插件市场</div>
+      <p>各类转账插件按需选购</p>
+    </div>
+    <div class="plugin-tab">
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="全部插件" name="all">
+          <div class="plugin-box-wrap">
+            <div class="plugin-box mr30">
+              <div class="plugin-box-content">
+                <img src="/imgs/orderReturn.png" width="300" height="200" alt="订单返还插件">
+                <div class="plugin-content-hide">
+                  <div class="title">订单返款插件</div>
+                  <p>适用于订单返款需求用户</p>
+                  <p>支持创建订单手机表单</p>
+                  <p>支持支付宝、微信批量返款</p>
+                  <p>支持授权自动审核以及手动审核</p>
+                  <el-button class="buyBtn" size="medium">立即购买</el-button>
+                </div>
+              </div>
+              <div class="pugin-info">
+                <p>订单返款插件</p>
+                <em>398元</em>
+              </div>
+            </div>
+            <div class="plugin-box mr30">
+              <div class="plugin-box-content">
+                <img src="/imgs/transferBlack.png" width="300" height="200" alt="转账黑名单功能">
+                <div class="plugin-content-hide">
+                  <div class="title">转账黑名单功能</div>
+                  <p>支持支付宝转账黑名单</p>
+                  <p>加入黑名单将无法转账</p>
+                  <el-button class="buyBtn" size="medium" @click="buy">立即购买</el-button>
+                </div>
+              </div>
+              <div class="pugin-info">
+                <p>转账黑名单功能</p>
+                <em>39.9元</em>
+              </div>
+            </div>
+            <div class="plugin-box">
+              <div class="plugin-box-content">
+                <img src="/imgs/message.png" width="300" height="200" alt="短信通知插件">
+                <div class="plugin-content-hide">
+                  <div class="title">短信通知插件</div>
+                  <p>自助购买短信0.05元/条</p>
+                  <p>如支付宝账号是手机号</p>
+                  <p>转账成功后自动发送短信</p>
+                  <el-button class="buyBtn" size="medium">立即购买</el-button>
+                </div>
+              </div>
+              <div class="pugin-info">
+                <p>短信通知插件</p>
+                <em>39.9元</em>
+              </div>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="已购买" name="bought">
+          <p>已购买</p>
+        </el-tab-pane>
+        <el-tab-pane label="未购买" name="buying">
+          <p>未购买</p>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-name: "index"
+  name: "index",
+  data() {
+    return {
+      activeName: 'all'
+    }
+  },
+  methods: {
+    buy() {
+      alert('购买插件')
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+@import "@/assets/scss/base.scss";
+@import "@/assets/scss/mixin.scss";
+
+.plugin-wrap {
+  padding: 20px 12%;
+  .plugin-title {
+    text-align: center;
+    .title{
+      margin-top: 20px;
+      font-weight: 400px;
+      font-style: normal;
+      font-size: 28px;
+    }
+    p{
+      margin-top: 30px;
+    }
+  }
+}
+.plugin-tab /deep/ {
+  margin-top: 40px;
+  .el-tabs__item {
+    &.is-active,
+    &:hover{color: $baseColor;}
+  }
+  .el-tabs__active-bar {
+    background-color: $baseColor;
+  }
+}
+.plugin-box-wrap {
+  display: flex;
+  .plugin-box {
+    border: 1px solid #e4ecf3;
+    padding: 10px;
+    .plugin-box-content {
+      position: relative;
+      width: 300px;
+      height: 200px;
+      overflow: hidden;
+      &:hover .plugin-content-hide{
+        top: 0;
+      }
+      .plugin-content-hide {
+        transition: all .3s linear;
+        position: absolute;
+        top: 100%;
+        width: 100%;
+        height: 100%;
+        background-color: #e4e4e4;
+        color: #999;
+        line-height: 1.5;
+        text-align: center;
+        padding: 10px;
+        .title {
+          font-weight: 400px;
+          font-style: normal;
+          font-size: 18px;
+          color: #000;
+          margin-bottom: 10px;
+        }
+        .buyBtn {
+          position: absolute;
+          bottom: 10px;
+          left: 50%;
+          transform: translateX(-50%);
+          color: #fff;
+          background-color: #f56c6c;
+          border-color: #f56c6c;
+        }
+      }
+    }
+    .pugin-info {
+      margin-top: 10px;
+      display: flex;
+      font-size: 14px;
+      em {
+        margin-left: auto;
+        font-style: normal;
+        color: rgb(255, 51, 0);
+      }
+    }
+  }
+}
+
 
 </style>
