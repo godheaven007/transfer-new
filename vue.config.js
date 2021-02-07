acf7bb64ffb893846fa3cb97dd1725b603531732
godheaven007 <345777292@qq.com>
2021-02-07 12:58:13 +0800

    documentã.title 设置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: '量付通'
+        }
+    },
     devServer: {
         proxy: {
             '/api': {
