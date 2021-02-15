9f6f329a519bf984841da314d3d142e6d334027b
godheaven_007 <345777292@qq.com>
2021-02-15 13:47:41 +0800

    批量上传接口不需要对参数进行JSON.stringify操作

--- src/util/request.js
+++ src/util/request.js
@@ -26,11 +26,11 @@ const service = axios.create({});
 
 service.interceptors.request.use(config => {
     //发请求前做的一些处理，数据转化，配置请求头，设置token,设置loading等，根据需求去添加
-    config.data = JSON.stringify(config.data); //数据转化,也可以使用qs转换
-    config.headers = {
-        'Content-Type':'application/x-www-form-urlencoded' //配置请求头
-    }
-    const token = Storage.getItem('token');
+    // config.data = JSON.stringify(config.data); //数据转化,也可以使用qs转换
+    // config.headers = {
+    //     'Content-Type':'application/x-www-form-urlencoded' //配置请求头
+    // }
+    const token = window.localStorage.getItem('token');
     if(token) {
         config.headers.Authorization = token;
     }
